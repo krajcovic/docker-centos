0003c3b938d05ad8e4a38b7b82696bf9393bc196
krajcovic <dusan.krajcovic@gmail.com>
2014-12-15 22:05:27 +0100

    Update scripts.

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run -ti -P krajcovic/centos:latest /bin/bash
+#docker run -ti -P --name console krajcovic/centos:latest /bin/bash
+docker run -d --name db training/postgres
+#docker run -d -P --name node1 krajcovic/centos:latest /bin/bash
+docker run -d -P --name web --link db:db training/webapp python app.py
--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
  #!/bin/bash
 
- docker run -d -P training/webapp python app.py
- docker run -d -p 5000:5000 training/webapp python app.py
+ #docker run -d -P training/webapp python app.py
+ #docker run -d -p 5000:5000 training/webapp python app.py
+ #docker run -d -p 127.0.0.1:5000:5000 training/webapp python app.py
+ #docker run -d -p 127.0.0.1::5000 training/webapp python app.py
+ #docker run -d -p 127.0.0.1:5000:5000/udp training/webapp python app.py
